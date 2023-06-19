340f3f64abc1e9bdac3ad350dbcfe1d3e1bfe052
unknown <adebayoaa519@gmail.com>
2023-06-19 16:42:15 +0100

    Completed day1 level 1

--- day1/level 1/main.js
+++ day1/level 1/main.js
@@ -38,3 +38,49 @@ lastItem=itCompanies.slice(itCompanies.length-1)
 for (const a of itCompanies) {
     console.log(a);
 }
+// Question 11 Change each company name to uppercase one by one and print them out
+for (const a of itCompanies) {
+    console.log(a.toUpperCase());
+}
+
+// Question 12 Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+console.log(itCompanies.join(", "));
+
+// Question 13 Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+console.log(itCompanies.includes("ase")?"ase":"is not found")
+
+// Question 14 Filter out companies which have more than one 'o' without the filter method
+for (let a = 0; a < itCompanies.length; a++) {
+        if(itCompanies[a].includes("oo")){
+            console.log(itCompanies[a]);
+    }
+}
+// Question 15 Sort the array using sort() method
+console.log(itCompanies.sort());
+
+//Question 16 Reverse the array using reverse() method
+console.log(itCompanies.reverse());
+
+//Question 17 Slice out the first 3 companies from the array
+console.log(itCompanies.slice(0,3));
+
+//Question 18 Slice out the last 3 companies from the array
+console.log(itCompanies.slice(4,itCompanies.length));
+
+//Question 19 Slice out the middle IT company or companies from the array
+let middle_element=Math.floor(itCompanies.length/2)
+console.log(middle_element);
+console.log(itCompanies.slice(middle_element,4));
+
+//Question 20 Remove the first IT company from the arra
+console.log(itCompanies.shift());
+
+//Question 21 Remove the middle IT company or companies from the array
+console.log(itCompanies.splice(middle_element,1));
+
+//Question 22 Remove the last IT company from the array
+console.log(itCompanies.pop());
+
+//Question 23 Remove all IT companies
+let Remove=itCompanies.splice()
+console.log(Remove);
